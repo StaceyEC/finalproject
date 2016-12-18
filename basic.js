d43664faf4cf792702693e141d7ff2780fe57c79
StaceyEC <stacey.clermont@gmail.com>
2016-12-18 00:31:39 -0500

    geo-code added

--- basic.js
+++ basic.js
@@ -1,4 +1,90 @@
-a
-   $(document).ready(function () {
-       $('.slider').slider({full_width: true});
-   });
+// if a map placeholder exists
+// initialize the map
+if ($('#map').length) {
+	// Initialize map
+	var map;
+	window.initMap = function () {
+		map = new google.maps.Map($('#map')[0], {
+			center: { lat: 40.7127837, lng: -74.00594130000002 },
+			zoom: 8
+		});
+	}
+
+
+	$(document).ready(function () {
+
+		// Initialize slider
+		$('.slider').slider({full_width: true});
+
+
+		// Set all map points
+		var geocoder = new google.maps.Geocoder();
+		window.volunteerDatabase.events.forEach(function(volunteerEvent) {
+
+			var searchString = volunteerEvent.company.address.street1 + " " + volunteerEvent.company.address.zipCode;
+
+			geocoder.geocode({'address': searchString }, function(results, status) {
+				if (status === 'OK') {
+					console.log("Search success! ", searchString)
+					map.setCenter(results[0].geometry.location);
+						var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					});
+				} else {
+					console.log('Search fail: ' + searchString + " "+ status);
+				}
+			});
+
+			// render cards
+
+		});
+
+
+	}); // end of document ready
+} 
+
+// If the page content has a location search
+// attach form listeners.
+if ($('#location').length) {
+
+	$('#location').keypress(onKeyPressed);
+	$("#location-btn").click(onKeyPressed);
+
+	function onKeyPressed(event) {
+		console.log(event);
+
+		if (event.which === 13) {
+			event.preventDefault();
+
+			let currentElement = $(this);
+
+			let userAddress = currentElement.val();
+			console.log('I got the address')
+
+			console.log(getURLExceptCurrentPage() + "index-results.html#" + encodeURI(userAddress) )
+
+			window.location.href = getURLExceptCurrentPage() + "index-results.html#" + userAddress;
+		}
+	}
+
+}
+
+function getURLExceptCurrentPage() {
+	let pathFragments = window.location.pathname.split('/');
+	let currentPage = pathFragments[pathFragments.length - 1];
+	return window.location.href.replace(currentPage, '');
+}
+
+/*
+		const container = $('.js-container');
+		container.html('');
+
+		const urlToRequest = `http://api.giphy.com/v1/gifs/search?q=${queryString}&api_key=dc6zaTOxFJmzC`;
+		console.log(urlToRequest)
+
+		const data = $.get( urlToRequest );
+
+		data.then(onDataBack);
+
+*/
